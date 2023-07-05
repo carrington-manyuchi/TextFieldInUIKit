3b19cac610ab22a41b00638b23f03cae65338c03
Carrington <Manyuchi>
2023-07-05 22:45:03 +0200

    add actions

--- TextFieldInUIKit/ViewController.swift
+++ TextFieldInUIKit/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    private let myTextField: UITextField = {
+        
+        let  textField = UITextField(frame: CGRect(x: 16, y: 243, width: 343, height: 200))
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        textField.textAlignment = NSTextAlignment.center
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.clearsOnBeginEditing = true
+        textField.adjustsFontSizeToFitWidth = true
+        textField.minimumFontSize = 15
+        textField.allowsEditingTextAttributes = true
+        textField.placeholder = "Please type something"
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = UIColor(white: 0.75, alpha: 0.75)
+        textField.leftView = UILabel(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+    textField.leftViewMode = .always
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(myTextField)
     }
 
 
